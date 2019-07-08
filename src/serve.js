adf8464b005695463c97c3ec0f7b7bf77ec7e559
lnsantos <39661894+lnsantos@users.noreply.github.com>
2019-07-08 04:33:18 -0300

    SQL

--- src/serve.js
+++ src/serve.js
@@ -3,12 +3,12 @@ const app = Express();
 
 app.get('/', (req, res) => {
   if (true) {
-    res.redirect('/inicio');
+    res.redirect('/inicio')
   }
-});
+})
 
 app.get('/inicio', (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
+  res.sendFile(`${__dirname}/public/index.html`)
 })
 
 module.exports = app
