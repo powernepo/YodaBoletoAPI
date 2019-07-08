da368f38dc41a32c4ee20b6054a257a3c9bcc03d
DonizeteVida <41035417+DonizeteVida@users.noreply.github.com>
2019-07-08 20:45:51 -0300

    Some erros

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,23 @@
 const basequery = require('../database/basequery')
 
 module.exports = {
-    async toRegister(user){ // O retorno vai ser uma promise, eu poderia usar o then(result=>{}) ou await basequery(...), que é como vamos fazer
-        //Como segundo parametro do nosso basequery, devemos passar um array. Algo assim ["Doni", 18, "Inteligente"] que bata com as interrogações
-        return await basequery("INSERT INTO usuario(usuario, nome, sobrenome,email,senha)" +
-        "VALUE (?,?,?,?,?)", user);
+    store(user) {
+        return basequery("INSERT INTO usuario(usuario, nome, sobrenome,email,senha)" +
+            "VALUE (?,?,?,?,?)", user);
     },
-    async toDelete(codigo){
-        return await basequery("DELETE FROM permissao WHERE codigo = ?", codigo);
+    delete(codigo) {
+        return basequery("DELETE FROM permissao WHERE codigo = ?", codigo);
     },
-    async toUpdate(user){
-        return await basequery("UPDATE usuario SET usuario = ?, nome = ? "+ 
-        " sobrenome = ?, email = ?, senha = ? ", user);
+    update(user) {
+        return basequery("UPDATE usuario SET usuario = ?, nome = ? " +
+            " sobrenome = ?, email = ?, senha = ? ", user);
     },
-    async informations(codigo){
-        return await basequery("SELECT * FROM usuario WHERE codigo = ?", codigo)
+    find(codigo) {
+        return basequery("SELECT * FROM usuario WHERE codigo = ?", codigo);
     },
-    async searchCode(user){ 
-        const code = await basequery("SELECT * FROM usuario" + 
-        " WHERE usuario = ? ,nome = ? ,sobrenome = ? ,email = ?", user)
-        return code
+    findBy(user) {
+        return basequery("SELECT * FROM usuario" +
+            " WHERE usuario = ? ,nome = ? ,sobrenome = ? ,email = ?", user);
+
     }
 }
