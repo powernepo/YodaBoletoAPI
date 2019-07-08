42d5ed16a91ff0f02f96ba4cfeb481b8e9949bc0
DonizeteVida <41035417+DonizeteVida@users.noreply.github.com>
2019-07-08 04:21:51 -0300

    Rota principal

--- src/serve.js
+++ src/serve.js
@@ -1,15 +1,8 @@
 const Express = require('express');
 const app = Express();
 
-let path = './public/index.html'
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-app.get('/', (req,res) => {
-    res.render('index.html')
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
 })
 
-  module.exports = app
+module.exports = app
