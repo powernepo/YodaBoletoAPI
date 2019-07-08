f915e476091b86f6d0a9a0e3c950b3bd2993c1c0
lnsantos <39661894+lnsantos@users.noreply.github.com>
2019-07-08 04:15:58 -0300

    BUGO

--- src/serve.js
+++ src/serve.js
@@ -1,10 +1,15 @@
-const Koa = require('koa');
-const app = new Koa();
+const Express = require('express');
+const app = Express();
 
 let path = './public/index.html'
 
-app.use('/home', ctx => {
-    ctx.redict(path)
-  });
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', (req,res) => {
+    res.render('index.html')
+})
 
   module.exports = app
