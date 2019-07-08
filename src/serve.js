e5ce3303afd8ae368e851a25396bf1852bc854b4
DonizeteVida <41035417+DonizeteVida@users.noreply.github.com>
2019-07-08 04:24:26 -0300

    Redirect

--- src/serve.js
+++ src/serve.js
@@ -2,6 +2,12 @@ const Express = require('express');
 const app = Express();
 
 app.get('/', (req, res) => {
+  if (true) {
+    res.redirect('/inicio');
+  }
+});
+
+app.get('/inicio', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 })
 
