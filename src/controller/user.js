802af93b35002cdbf8dc090aa71af2ea2ce3a89d
DonizeteVida <41035417+DonizeteVida@users.noreply.github.com>
2019-07-08 21:20:16 -0300

    Try catch

--- src/controller/user.js
+++ src/controller/user.js
@@ -3,7 +3,14 @@ const user = require('../models/user')
 module.exports = {
     async novo(req, res) {
         const { usuario, nome, sobrenome, email, senha } = req.body;
-        const result = await user.store([usuario, nome, sobrenome, email, senha]);
+        try {
+            const result = await user.store([usuario, nome, sobrenome, email, senha]);
+
+            res.status(200).send("SUCESSO");
+
+        } catch (error) {
+            res.status(404).send(error);
+        };
     },
     perfil(req, res) {
         res.send("perfil");
