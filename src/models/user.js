d3785926f496641b0d77be201e72036b2bb6349b
DonizeteVida <41035417+DonizeteVida@users.noreply.github.com>
2019-07-09 18:22:51 -0300

    boleto

--- src/models/user.js
+++ src/models/user.js
@@ -2,22 +2,18 @@ const basequery = require('../database/basequery')
 
 module.exports = {
     store(user = []) {
-        return basequery("INSERT INTO usuario(usuario, nome, sobrenome,email,senha)" +
-            "VALUE (?,?,?,?,?)", user);
+        return basequery("INSERT INTO usuario(usuario, nome, sobrenome, email, senha) VALUE (?,?,?,?,?)", user);
     },
     delete(codigo = []) {
-        return basequery("DELETE FROM permissao WHERE codigo = ?", codigo);
+        return basequery("DELETE FROM usuario WHERE codigo = ?", codigo);
     },
     update(user = []) {
-        return basequery("UPDATE usuario SET usuario = ?, nome = ? " +
-            " sobrenome = ?, email = ?, senha = ? ", user);
+        return basequery("UPDATE usuario SET usuario = ?, nome = ?, sobrenome = ?, email = ?, senha = ? WHERE codigo = ?", user);
     },
     findById(codigo = []) {
         return basequery("SELECT * FROM usuario WHERE codigo = ?", codigo);
     },
-    findBy(user = []) {
-        return basequery("SELECT * FROM usuario" +
-            " WHERE usuario = ? ,nome = ? ,sobrenome = ? ,email = ?", user);
-
+    findByUser(user= []){
+        return basequery("SELECT * FROM usuario AS u WHERE u.usuario = ?", user);
     }
 }
