62747cb62cf9a336195cf83a3ee35a6b1658aba9
lnsantos <39661894+lnsantos@users.noreply.github.com>
2019-07-09 01:34:34 -0300

    NOVO MÉTODO

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,11 +1,23 @@
 const user = require('../models/user')
 const bcrypt = require('bcryptjs')
+
+function verificaSenha (reqPasw,hashDB){ //Não esta funfando
+    bcrypt.compare(reqPasw,hashDB,(err, resut)=>{
+        if(err) throw err
+        else return resut > 1
+    })
+}
 module.exports = {
     async novo(req, res) {
         const { usuario, nome, sobrenome, email, senha } = req.body;   
+       
         try {
             bcrypt.hash(req.body.senha,10, async (err,hash)=>{
-                await user.store([usuario, nome, sobrenome, email, hash]);
+                console.log( verificaSenha(req.body.senha,hash))
+                await user.store([usuario, nome, sobrenome, email, hash])
+                    .catch(()=>{
+                        res.status(401).send("USUÁRIO/EMAIL JÁ EXISTE");
+                    })
                 res.status(200).send("SUCESSO");
             })
         } catch (error) {
@@ -20,7 +32,8 @@ module.exports = {
             res.status(404).send(error);
         }
     },
+      
     atualizar(req, res) {
-
+       
     }
 }
